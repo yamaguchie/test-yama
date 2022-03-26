4b0cc5d817273567d95b89915f1525731c295efe
plentyomoide@gmail.com <plentyomoide@gmail.com>
2022-03-26 04:58:14 +0000

    xxxx

--- test_yama/import-test.py
+++ test_yama/import-test.py
@@ -1,6 +1,8 @@
 # Databricks notebook source
-try:
-  from test_yama.jobs.sample.entrypoint import SampleJob
-except:
-  pass
+spark = SparkSession.builder.getOrCreate()
+app_name = spark.conf.get("spark.app.name")
+if app_name == 'Databricks Shell':
+  print("Databricks Notebook Execute")
   %run "../../test_yama/jobs/sample/entrypoint"
+else:
+  from test_yama.jobs.sample.entrypoint import SampleJob
