5e565cd7544e45819bd20dfcc3e3aa668a0bcd20
plentyomoide@gmail.com <plentyomoide@gmail.com>
2022-03-26 05:03:08 +0000

    ssssss

--- test_yama/import-test.py
+++ test_yama/import-test.py
@@ -1,8 +1,14 @@
 # Databricks notebook source
+from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
 app_name = spark.conf.get("spark.app.name")
-if app_name == 'Databricks Shell':
+if app_name == 'Databricks Sahell':
   print("Databricks Notebook Execute")
   %run "../../test_yama/jobs/sample/entrypoint"
 else:
-  from test_yama.jobs.sample.entrypoint import SampleJob
+  import os
+  #from test_yama.jobs.sample.entrypoint import SampleJob
+
+# COMMAND ----------
+
+# MAGIC %whos
