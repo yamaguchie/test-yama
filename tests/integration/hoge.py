edacb223fdcd8de1f7b0da3e5f3e457caa782cef
yamaguchie <plentyomoide@gmail.com>
2022-04-03 18:19:06 +0900

    aaaa

--- tests/integration/hoge.py
+++ tests/integration/hoge.py
@@ -2,6 +2,7 @@
 ## Databricks notebook source
 import unittest
 
+
 try:
   from test_yama.jobs.sample.entrypoint import SampleJob
 except:
--- tests/integration/moe.py
+++ tests/integration/moe.py
@@ -1,3 +1,9 @@
 # Databricks notebook source
 def llll():
   print("aaaa")
+
+
+
+import os
+
+os.path.basename("yama")
